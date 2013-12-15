b9cb1bb8a9e5e56430a44941a7d794ce1c79d78c
Billy Bob <Billy Bob@PrettyBlade.gateway.2wire.net>
2013-12-15 14:53:12 -0800

    IceLevel Win Condition works, need to update StatusText

--- src/ssk/project/Basic_Pong/Level_Ice/IceLevel1.java
+++ src/ssk/project/Basic_Pong/Level_Ice/IceLevel1.java
@@ -2,6 +2,7 @@ package ssk.project.Basic_Pong.Level_Ice;
 import java.util.ArrayList;
 import java.util.List;
 
+import ssk.project.Basic_Pong.Modular.BaseActivity;
 import ssk.project.Basic_Pong.Modular.BaseLevel;
 import ssk.project.GameUnits.IceBlock;
 import ssk.project.GameUnits.SolidBlock;
@@ -11,16 +12,16 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 
 
-public class IceGameView extends BaseLevel {
+public class IceLevel1 extends BaseLevel {
 	
-	int iceHit;
 	List<IceBlock> iceBlocks = new ArrayList<IceBlock>();
 	// Level Variables
-	int iceblockQuantity = 16;
+	int iceHit = 0;
+	int iceblockQuantity = 8;
 	IceBlock ib;
 	SolidBlock sb;
 		
-	public IceGameView(Context context) {
+	public IceLevel1(Context context) {
 		super(context);
 		bgBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ice_cave);
 	}
@@ -28,23 +29,40 @@ public class IceGameView extends BaseLevel {
 	@Override
 	public void onSizeChanged(int w, int h, int oldw, int oldh) {
 		super.onSizeChanged(w, h, oldw, oldh);
-		
+		gt.setIceLevel();
 		ib = new IceBlock(this, context, screenW, screenH);
-		for (int i = 0; i < (iceblockQuantity / 2); i++) {
+		for (int i = 0; i < (iceblockQuantity); i++) {
 			IceBlock ice = new IceBlock(ib, i * ib.w + 10, screenH / 2);
 			iceBlocks.add(ice);
 		}
 		sb = new SolidBlock(this, context, screenW, screenH);
 	}
 	
+	public void updateIceBlocks(Canvas canvas) {
+		for (int i = 0; i < iceBlocks.size(); i++) {
+			iceBlocks.get(i).update2(canvas, ib);
+		}
+		for (int i = 0; i < iceBlocks.size(); i++) {
+			if (iceBlocks.get(i).bounceBall2(b, ib)) {
+				iceHit++;
+			}
+		}
+	}
+	
 	@Override
 	public void draw(Canvas canvas) {
 		super.draw(canvas);	
-		// Drawing IceBlocks
-		for (int i = 0; i < iceBlocks.size(); i++) {
-			iceBlocks.get(i).update2(canvas, b, ib);
-		}		
+		updateIceBlocks(canvas);
 		// Drawing SolidBlock
 		sb.update(canvas, b);
+	}
+	
+	@Override
+	public void winCondition() {
+		if (iceHit == iceblockQuantity * 2) {
+			thread.setRunning(false);
+			pause = true;
+			((BaseActivity)getContext()).winScreen();
+		}
 	}
 }
