3d34ac952da7db884685c20916163b6fcb79cab1
Billy Bob <Billy Bob@PrettyBlade.gateway.2wire.net>
2013-12-16 10:01:31 -0800

    Warning Text works

--- src/ssk/project/Basic_Pong/Level_Wood/WoodLevel1.java
+++ src/ssk/project/Basic_Pong/Level_Wood/WoodLevel1.java
@@ -2,6 +2,7 @@ package ssk.project.Basic_Pong.Level_Wood;
 import ssk.project.BaseClasses.BaseActivity;
 import ssk.project.BaseClasses.BaseLevel;
 import ssk.project.GameUnits.Lightning;
+import ssk.project.GameUnits.WarningText;
 import ssk.project.Pong_Basic.R;
 import android.content.Context;
 import android.graphics.BitmapFactory;
@@ -11,6 +12,7 @@ import android.graphics.Canvas;
 public class WoodLevel1 extends BaseLevel {
 
 	Lightning li;
+	WarningText wt;
 	
 	public WoodLevel1(Context context) {
 		super(context);
@@ -20,12 +22,14 @@ public class WoodLevel1 extends BaseLevel {
 	@Override
 	public void onSizeChanged(int w, int h, int oldw, int oldh) {
 		super.onSizeChanged(w, h, oldw, oldh);
+		wt = new WarningText(screenW, screenH);
 		li = new Lightning(this, context, screenW, screenH);
 	}
 	
 	@Override
 	public void draw(Canvas canvas) {
 		super.draw(canvas);	
+		wt.updateText(canvas);
 		li.update(canvas, p);
 	}
 	
