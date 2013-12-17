f08e66312db08bd8b2f5fd8ae54c99edad1a9329
Billy Bob <Billy Bob@PrettyBlade.gateway.2wire.net>
2013-12-17 12:50:43 -0800

    Decided to fix the sound issue, before the PowerUp Issue

--- src/ssk/project/Basic_Pong/Level_Wood/WoodLevel2.java
+++ src/ssk/project/Basic_Pong/Level_Wood/WoodLevel2.java
@@ -1,16 +1,13 @@
 package ssk.project.Basic_Pong.Level_Wood;
 import ssk.project.BaseClasses.BaseActivity;
 import ssk.project.GameUnits.Lightning;
-import ssk.project.GameUnits.PowerUpSlow;
 import android.content.Context;
 import android.graphics.Canvas;
-import android.view.MotionEvent;
 
 
 public class WoodLevel2 extends WoodLevel1 {
 
 	Lightning li2;
-	PowerUpSlow pus;
 	
 	public WoodLevel2(Context context) {
 		super(context);
@@ -20,21 +17,12 @@ public class WoodLevel2 extends WoodLevel1 {
 	public void onSizeChanged(int w, int h, int oldw, int oldh) {
 		super.onSizeChanged(w, h, oldw, oldh);
 		li2 = new Lightning(li);
-		pus = new PowerUpSlow(this, context, screenW, screenH);
-	}
-	
-	public synchronized boolean onTouchEvent(MotionEvent event) {
-		super.onTouchEvent(event);
-		pus.onClick(event);
-		return true;
 	}
 	
 	@Override
 	public void draw(Canvas canvas) {
 		super.draw(canvas);	
 		li2.update2(canvas, p, li);
-		levelEvent();
-		pus.update(canvas, li2);
 	}
 	
 	@Override
@@ -44,12 +32,6 @@ public class WoodLevel2 extends WoodLevel1 {
 			thread.setRunning(false);
 			pause = true;
 			((BaseActivity)getContext()).loseScreen();
-		}
-	}
-	
-	public void levelEvent() {
-		if (ballHits == 3 && pus.isAvailable()) {
-			pus.show();			
 		}
 	}
 }
